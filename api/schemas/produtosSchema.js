3042272a301d2f004a94a478675e38da851ed1a6
Ygor Fortes <ygor1888@hotmail.com>
2023-11-08 13:40:57 -0300

    feat: endpoint de cadastrar produtos criado em controller produtos

--- api/schemas/produtosSchema.js
+++ api/schemas/produtosSchema.js
@@ -1,17 +1,20 @@
 import yup from 'yup';
+
+
 const registroProdutosSchema = yup.object({
   body: yup.object({
-    descricao: yup.string().trim().required('O campo descricao é obrigatório.').max(45)
-    // quantidade_estoque: yup.number().integer('O campo quantidade_estoque só recebe número inteiros')
-    //   .positive('O campo quantidade_estoque só recebe número positivos')
-    //   .required('O campo quantidade_estoque é obrigatório.'
-    // ),
-    // valor: yup.number().positive('O campo valor só recebe número positivos').required('O campo valor é obrigatório.'),
-    // categoria_id: yup.number('O campo categoria_id só recebe números')
-    //   .integer('O campo categoria_id só recebe número inteiros')
-    //   .positive('O campo categoria_id só recebe número positivos')
-    //   .required('O campo categoria_id é obrigatório.'
-    // )
+    descricao: yup.string().trim().required('O campo descricao é obrigatório.').max(45),
+    quantidade_estoque: yup.number().integer('O campo quantidade_estoque só recebe número inteiros')
+      .positive('O campo quantidade_estoque só recebe número positivos')
+      .required('O campo quantidade_estoque é obrigatório.'
+    ),
+    valor: yup.number().positive('O campo valor só recebe número positivos').required('O campo valor é obrigatório.'),
+    categoria_id: yup.number()
+      .typeError('O campo categoria_id só recebe números')
+      .integer('O campo categoria_id só recebe número inteiros')
+      .positive('O campo categoria_id só recebe número positivos')
+      .required('O campo categoria_id é obrigatório.'
+    )
   }),
 })
 
