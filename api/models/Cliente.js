75736765a3d7045adf9675a7c243315d1ca0bbc7
Ygor Fortes <ygor1888@hotmail.com>
2023-11-21 15:06:04 -0300

    refac: refatorado cliente,e  criado model de cliente

---
+++ api/models/Cliente.js
@@ -0,0 +1,91 @@
+import db from "../db/conecaodb.js";
+
+class Cliente {
+  constructor({
+    id,
+    nome,
+    email,
+    cpf,
+    cep, 
+    rua, 
+    numero,
+    bairro,
+    cidade,
+    estado,
+    created_at,
+    updated_at,
+    deletedAt,
+  }){
+    this.id = null || id;
+    this.nome = nome,
+    this.email = email;
+    this.cpf = cpf;
+    this.cep = null || cep;
+    this.rua  = null || rua;
+    this.numero = null || numero;
+    this.bairro = null || bairro;
+    this.cidade = null || cidade;  
+    this.estado = null || estado;
+    this.created_at = created_at || new Date();
+    this.updated_at = updated_at || new Date();
+    this.deletedAt = null || deletedAt;
+  }
+
+  static async pegar(){
+    return db('clientes').whereNull('deletedAt');
+  }
+
+  static async pegarPorId(id){
+    return db('clientes').where(id).whereNull('deletedAt');
+  }
+
+  async criar(){
+    const [idClienteCriado] = await db('clientes').insert(this);
+    const [clienteCriado] = await db('clientes').where({id: idClienteCriado});
+    return new Cliente(clienteCriado);
+  }
+
+  async atualizar(id){
+    await db('clientes').update({...this, updated_at: new Date()}).where('id', id);
+    return db('clientes').where('id', id);
+  }
+
+
+  async salvar(){
+    if(this.id){
+      const resultado = await this.atualizar(this.id);
+      return resultado;
+    }
+    const resultado = await this.criar();
+    return resultado;
+  }
+
+
+
+  static async verificaEmail(email){
+    return db('clientes').where(email);
+  }
+
+  static async verificaCpf(cpf){
+    return db('clientes').where(cpf);
+  }
+
+
+
+  static async verificaEmailCpfUnico(email, cpf, id = null){
+    const resultado =  db('clientes').where((function (){
+      this.where({email: email})
+      .orWhere({cpf: cpf})
+    }))
+    .whereNull('deletedAt');
+
+    if(id){
+      resultado.andWhereNot({id});
+    }
+    return resultado;
+  }
+
+
+}
+
+export default Cliente;
--- api/services/ClientesServices.js
+++ api/services/ClientesServices.js
@@ -1,26 +1,71 @@
-import db from "../db/conecaodb.js";
-import { Services } from "./services.js";
-
-class ClientesServices extends Services{
-  constructor(){
-    super('clientes');
+import Cliente from "../models/Cliente.js";
+import ErroCustomizado from "../erros/ErroCustomizado.js";
+class ClientesServices  {
+  
+  async listarClientes(){
+    try {
+      const resultado = await Cliente.pegar();
+      return resultado;
+    } catch (erro) {
+      throw erro;
+    }
   }
 
-  async verificaUnidadeEmailCpf(email, cpf){
-    return db(this.nomeModelo).where((function (){
-      this.where({email: email})
-      .orWhere({cpf: cpf})
-    }))
-    .whereNull('deletedAt')
+  async listarClientePorId(id){
+    try {
+      const resultado = Cliente.pegarPorId({id});
+      return resultado;
+    } catch (erro) {
+      throw erro;
+    }
   }
 
-  async veirificaEmailCpfUnico(email, cpf, idCliente){
-    return db(this.nomeModelo).where((function (){
-      this.where({email: email})
-      .orWhere({cpf: cpf})
-    }))
-    .whereNull('deletedAt').andWhereNot({id: idCliente});
+  async  criarCliente(clienteCorpo){
+    const {email, cpf} = clienteCorpo;
+    try {
+      await verificarEmailCpfUnicos(email, cpf);
+
+      const cliente = new Cliente(clienteCorpo);
+      const resultado = await cliente.salvar();
+      return resultado;
+    } catch (erro) {
+      throw erro;
+    }
   }
+
+  async atualizarCliente(id, clienteCorpo){
+    const {email, cpf} = clienteCorpo;
+    try {
+      await verificarEmailCpfUnicos(email, cpf, id);
+      const clienteNovo = new Cliente({id, ...clienteCorpo} );
+      const resultado = await clienteNovo.salvar();
+      return resultado;
+    } catch (erro) {
+      console.log(erro)
+      throw erro;
+    }
+  }
+
+
+}
+
+async function verificarEmailCpfUnicos(email, cpf, id = null){
+
+  try {
+    const [unidadeCadastrada] = await Cliente.verificaEmailCpfUnico(email, cpf, id);
+    if(unidadeCadastrada){
+      const mensagens = {
+        email: 'Email já cadastrado.',
+        cpf: 'Cpf já cadastrado.' , 
+        ambos:  'Cpf e email já cadastrados.' 
+      }
+      const mensagem = mensagens[unidadeCadastrada.email === email && unidadeCadastrada.cpf === cpf ? 'ambos' : unidadeCadastrada.email === email ? 'email': 'cpf'];
+      throw new ErroCustomizado(mensagem, 409);
+    }
+  } catch (erro) {
+    throw erro;
+  }
+  
 }
 
 export default ClientesServices;
