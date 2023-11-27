9c81cc2d5adfb971e1bb6555c1dc70cfd27579b4
Ygor Fortes <ygor1888@hotmail.com>
2023-11-27 18:15:28 -0300

    chore: As váriveis que estavam como resultado, foram renomeadas como cliente, ou clientes conforme o contexto

--- api/services/ClientesServices.js
+++ api/services/ClientesServices.js
@@ -4,8 +4,8 @@ class ClientesServices  {
   
   async listarClientes(){
     try {
-      const resultado = await Cliente.pegar();
-      return resultado;
+      const clientes = await Cliente.pegar();
+      return clientes;
     } catch (erro) {
       throw erro;
     }
@@ -13,37 +13,54 @@ class ClientesServices  {
 
   async listarClientePorId(id){
     try {
-      const resultado = Cliente.pegarPorId({id});
-      return resultado;
+      const [cliente] = await Cliente.pegarPorId({id});
+
+      if(!cliente){
+        return [];
+      }
+
+      return cliente;
     } catch (erro) {
       throw erro;
     }
+
   }
 
-  async  criarCliente(clienteCorpo){
-    const {email, cpf} = clienteCorpo;
+  async  criarCliente(dadosCliente){
+    const {email, cpf} = dadosCliente;
     try {
       await verificarEmailCpfUnicos(email, cpf);
 
-      const cliente = new Cliente(clienteCorpo);
-      const resultado = await cliente.salvar();
-      return resultado;
+      const clienteNovo = new Cliente(dadosCliente);
+      const clienteCadastrado = await clienteNovo.salvar();
+
+      return {mensagem: 'Cliente cadastrado', cliente: clienteCadastrado};
     } catch (erro) {
       throw erro;
     }
+
   }
 
-  async atualizarCliente(id, clienteCorpo){
-    const {email, cpf} = clienteCorpo;
+  async atualizarCliente(id, dadosCliente){
+    const {email, cpf} = dadosCliente;
     try {
+
+      const [clienteAtual] = await Cliente.pegarPorId({id});
+      if(!clienteAtual){
+        throw new ErroCustomizado('Cliente não encontrado', 404);
+      }
+
       await verificarEmailCpfUnicos(email, cpf, id);
-      const clienteNovo = new Cliente({id, ...clienteCorpo} );
-      const resultado = await clienteNovo.salvar();
-      return resultado;
+
+      const clienteAtualizado = new Cliente({id: clienteAtual.id, ...dadosCliente} );
+
+      const clienteSalvo = await clienteAtualizado.salvar();
+
+      return {mensagem: 'Cliente cadastrado', cliente: clienteSalvo};
     } catch (erro) {
-      console.log(erro)
       throw erro;
     }
+
   }
 
 
