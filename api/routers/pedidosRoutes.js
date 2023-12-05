040e2fbf2052a90edc9069d0d2df7492908919d1
Ygor Fortes <ygor1888@hotmail.com>
2023-12-05 07:40:16 -0300

    chore: Adicionado endopoint get /pedido

--- api/routers/pedidosRoutes.js
+++ api/routers/pedidosRoutes.js
@@ -3,6 +3,7 @@ import verificarToken from "../middlewares/verificarToken.js";
 import PedidosController from "../controller/PedidosController.js";
 const router = Router();
 
+router.get('/pedido', PedidosController.listarPedido)
 router.post('/pedido', PedidosController.cadastrarPedidos)
 
 
