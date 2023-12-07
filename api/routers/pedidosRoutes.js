78671731f8bbc939994be030295a94bc4579b925
Ygor Fortes <ygor1888@hotmail.com>
2023-12-07 09:00:53 -0300

    chore: adicionado metodo de verificarToken nas rotas de pedidos

--- api/routers/pedidosRoutes.js
+++ api/routers/pedidosRoutes.js
@@ -3,8 +3,8 @@ import verificarToken from "../middlewares/verificarToken.js";
 import PedidosController from "../controller/PedidosController.js";
 const router = Router();
 
-router.get('/pedido', PedidosController.listarPedido)
-router.post('/pedido', PedidosController.cadastrarPedidos)
+router.get('/pedido', verificarToken, PedidosController.listarPedido)
+router.post('/pedido',verificarToken, PedidosController.cadastrarPedidos)
 
 
 export default router;
